6cf40de7c5726ebc8805897e647152e5080c2829
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-01-07 18:15:15 -0500

    URL configuration

---
+++ digitallibrary/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from digitallibrary.api.views import *
+
+urlpatterns = [
+    path('authors', AuthorViewSet.as_view(), name='Autores'),
+    path('author/<int:pk>/', AuthorDetailAPIView.as_view(), name='Autor'),
+    path('authors/create', AuthorCreateAPIView.as_view(), name='Crear autor'),
+    path('authors/delete/<int:pk>/', AuthorDeleteAPIView.as_view(), name='Eliminar autor'),
+    path('authors/update/<int:pk>/', AuthorUpdateAPIView.as_view(), name='Actualizar autor'),
+    path('books', BooksViewSet.as_view(), name='Libros'),
+    path('books/create', BooksCreateAPIView.as_view(), name='Crear libro'),
+    path('book/delete/<int:pk>', BookDeleteAPIView.as_view(), name='Eliminar libro'),
+    path('book/update/<int:pk>/', BookUpdateAPIView.as_view(), name='Actualizar libro'),
+    ]
